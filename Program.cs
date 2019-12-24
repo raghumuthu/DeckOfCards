321fb0954bcefe98812ac3763d19a9766893d203
raghumuthu <raghu.marimuthu@gmail.com>
2019-12-24 17:00:26 +0800

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DeckofCards.Model;
+
+namespace DeckofCards
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Deck cardDeck = new Deck();
+
+            //Shuffling the Deck
+            cardDeck.Shuffle();
+
+            //Sorting the Deck
+            CardSorter sorter = new CardSorter(CardSortType.SuitThenKind);
+            cardDeck.Sort(sorter);
+
+            //Deal the top card of Deck
+            cardDeck.DrawTopCard();
+
+            //Shuffle the Deck
+            cardDeck.Shuffle();
+            CardSorter cardsorter = new CardSorter(CardSortType.SuitThenKind);
+            cardDeck.Sort(cardsorter);
+
+            // Keep console open until a key is pressed
+            Console.ReadKey();
+        }
+    }
+
+
+}
